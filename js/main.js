8fdfbe3c0f67b78b57b043d200605c228c0764c5
Robie <robie1373@gmail.com>
2018-08-31 18:18:41 +0000

    updating 8-31-2018

--- js/main.js
+++ js/main.js
@@ -4,32 +4,32 @@ import ExampleWorks from './example-work';
 
 const myWork = [
   {
-    'title': "This is not my Work Example",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'title': "Placeholder 1.",
+    'href': "https://portfolio.robielutsey.com",
+    'desc': "Updates coming soon. Thank you for your patience.",
     'image': {
-      'desc': "example screenshot of a project involving code",
+      'desc': "Fancy placeholder. Not as good as the real thing, but ok.",
       'src': "images/example1.png",
-      'comment': ""
+      'comment': "Under construction"
     }
   },
   {
-    'title': "This is not my Work Example either",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'title': "Placeholder 2.",
+    'href': "https://portfolio.robielutsey.com",
+    'desc': "Updates coming soon. Thank you for your patience.",
     'image': {
-      'desc': "example screenshot of a project involving chemistry",
+      'desc': "Fancy placeholder. Not as good as the real thing, but ok.",
       'src': "images/example2.png",
       'comment': `“Chemistry” by Surian Soosay is licensed under CC BY 2.0
            https://www.flickr.com/photos/ssoosay/4097410999`
     }
   },
   {
-    'title': "Also not my Work Example",
-    'href': "https://example.com",
-    'desc': "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    'title': "Placeholder 3.",
+    'href': "https://portfolio.robielutsey.com",
+    'desc': "Updates coming soon. Thank you for your patience.",
     'image': {
-      'desc': "example screenshot of a project involving cats",
+      'desc': "Fancy placeholder. Not as good as the real thing, but ok.",
       'src': "images/example3.png",
       'comment': `“Bengal cat” by roberto shabs is licensed under CC BY 2.0
            https://www.flickr.com/photos/37287295@N00/2540855181`
